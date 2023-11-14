7d07ef997e4a3936f729fa9b31e1658436f4020e
Jeison Steven Florez Julio <000525516@bga.upb>
2023-11-14 07:45:14 -0500

    I added some tests c:

--- src/test/java/LoanManagerTest.java
+++ src/test/java/LoanManagerTest.java
@@ -1,12 +1,32 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author 000525516
- */
 public class LoanManagerTest {
-    
+    @Test
+    public void testAddBook() {
+        System.out.println("addBook");
+        Book book = null;
+        LoanManager instance = null;
+        instance.addBook(book);
+        fail("The test case is a prototype.");
+    }
+
+    @Test
+    public void testRegisterStudent() {
+        System.out.println("registerStudent");
+        Student student = null;
+        LoanManager instance = null;
+        instance.registerStudent(student);
+        fail("The test case is a prototype.");
+    }
+
+    @Test
+    public void testLendBook() {
+        System.out.println("lendBook");
+        String bookId = "";
+        String studentId = "";
+        LoanManager instance = null;
+        boolean expResult = false;
+        boolean result = instance.lendBook(bookId, studentId);
+        assertEquals(expResult, result);
+        fail("The test case is a prototype.");
+    }
 }
